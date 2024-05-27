0f4ab4e877ea848838ff649b53127a0b966e526f
Patrick Barker <patrickbarkerco@gmail.com>
2024-05-27 13:21:29 -0600

    update devices

--- skillpacks/server/models.py
+++ skillpacks/server/models.py
@@ -1,7 +1,7 @@
-from typing import Optional, Dict, Any, List
+from typing import Any, Dict, List, Optional
 
-from pydantic import BaseModel, Field
 from mllm import V1Prompt
+from pydantic import BaseModel, Field
 from toolfuse.models import V1ToolRef
 
 
